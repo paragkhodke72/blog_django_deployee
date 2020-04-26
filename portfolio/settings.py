00b461411aee5532aaf7c3273bd191e599747221
parag khodke <paragkhodke72@gmail.com>
2020-04-26 15:46:04 -0400

    first commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles', 'blog',
+    'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -112,6 +113,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
